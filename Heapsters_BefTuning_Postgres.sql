c3ae715adfcc4fc4bb1a4688b45292d4c12148a5
EMILY JACKSON <emily@nike.cs.uga.edu>
2017-04-13 21:22:08 -0400

    BefTuning_Postgres updated

--- Heapsters_BefTuning_Postgres.sql
+++ Heapsters_BefTuning_Postgres.sql
@@ -1,79 +1,42 @@
-reset query cache;
-use Heapsters_Tuning_MySQL;
+-- Queries for Project 4
+-- -------------------------------------
 
--- List name of the student with id equal to v1 (id)
+-- 1. List the name of the student with id equal to v1 (id).
+SELECT stud_name FROM student WHERE stud_id = 774861;
 
-EXPLAIN
-SELECT name
-FROM Student
-WHERE id = 912325;
+-- 2. List the names of students with id in the range of v2 (id) to v3 (inclusive).
+SELECT stud_name FROM student WHERE stud_id BETWEEN 396624 AND 763176;
 
-SELECT name
-FROM Student
-WHERE id = 912325;
+-- 3. List the names of students who have taken course v4 (crs_code).
+SELECT stud_name FROM student
+JOIN transcript
+ON transcript.stud_id = student.stud_id
+WHERE transcript.crs_code = '917489';
 
--- List names of students with id in range of v2 (id) to v3 (inclusive).
+-- 4. List the names of students who have taken a course taught by professor v5 (name).
+SELECT DISTINCT student.stud_name FROM student
+JOIN transcript
+ON transcript.stud_id = student.stud_id
+JOIN teaching
+ON teaching.crs_code = transcript.crs_code AND teaching.semester = transcript.semester
+JOIN professor
+ON professor.Id = teaching.prof_id
+WHERE professor.Name = 'name10747';
 
-EXPLAIN
-SELECT name
-FROM Student
-WHERE id >= 3000 AND id <= 9000;
+-- 5. List the names of students who have taken a course from department v6 (dept_id), but not v7.
+SELECT stud_name FROM student, 
+       (SELECT stud_id FROM transcript, course WHERE dept_id = '160435' AND course.crs_code = transcript.crs_code
+	AND stud_id NOT IN
+	(SELECT stud_id FROM transcript, course WHERE dept_id = '78889' AND course.crs_code = transcript.crs_code)) as alias
+       WHERE student.stud_id = alias.stud_id;
 
-SELECT name
-FROM Student
-WHERE id >= 3000 AND id <= 9000;
-
--- List names of students who have taken course v4 (crsCode)
-
-EXPLAIN
-SELECT S.name
-FROM Student AS S, Transcript AS T
-WHERE S.id = T.studId && T.crsCode = 'crs148793';
-
-SELECT S.name
-FROM Student AS S, Transcript AS T
-WHERE S.id = T.studId && T.crsCode = 'crs148793';
-
-SELECT S.name
-FROM Student AS S, Transcript AS T
-WHERE S.id = T.studId && T.crsCode = 'crs148793';
-
--- List of names of students who have taken a course taught by professor v5 (name)
-
-EXPLAIN
-SELECT S.name
-FROM Student AS S, Transcript AS Tr, Teaching AS Te, Professor AS P
-WHERE S.id = Tr.studId && Tr.crsCode = Te.crsCode && Te.profId = P.id && P.name = 'name489302';
-
-SELECT S.name
-FROM Student AS S, Transcript AS Tr, Teaching AS Te, Professor AS P
-WHERE S.id = Tr.studId && Tr.crsCode = Te.crsCode && Te.profId = P.id && P.name = 'name489302';
-
--- List of names of students who have taken a course from department v6 (deptId), but not v7
-
-EXPLAIN
-SELECT S.name
-FROM Student S, Transcript Tr, Teaching Te, Professor P
-WHERE S.id = Tr.studId && Tr.crsCode = Te.crsCode && Te.profId = P.id && P.deptId = 6 && P.deptId != 7;
-SELECT S.name
-FROM Student S, Transcript Tr, Teaching Te, Professor P
-WHERE S.id = Tr.studId && Tr.crsCode = Te.crsCode && Te.profId = P.id && P.deptId = 6 && P.deptId != 7;
-
--- List names of students who have taken all courses offered by department v8 (deptId)
-
-EXPLAIN
-SELECT S.name
-FROM Student AS S, Transcript AS Tr, Teaching AS Te, Professor P
-WHERE S.id = Tr.studId && Tr.crsCode = Te.crsCode && Te.profId = P.id
-GROUP BY S.name, S.id
-HAVING COUNT(*) = (SELECT COUNT(*)
-    FROM Professor AS P JOIN Teaching AS T
-    WHERE P.id = T.profId && P.deptId = 'dept873573');
-
-SELECT S.name
-FROM Student AS S, Transcript AS Tr, Teaching AS Te, Professor P
-WHERE S.id = Tr.studId && Tr.crsCode = Te.crsCode && Te.profId = P.id
-GROUP BY S.name, S.id
-HAVING COUNT(*) = (SELECT COUNT(*)
-    FROM Professor AS P JOIN Teaching AS T
-    WHERE P.id = T.profId && P.deptId = 'dept873573');
+       -- 6. List the names of students who have taken all courses offered by department v8 (dept_id).
+       SELECT stud_name 
+       FROM student
+       JOIN transcript
+       ON student.stud_id = transcript.stud_id
+       WHERE crs_code IN
+       (SELECT crs_code FROM course WHERE dept_id = '14420' AND crs_code IN (SELECT crs_code FROM teaching))
+       GROUP BY stud_name
+       HAVING COUNT(*) = 
+       (SELECT COUNT(*) FROM course WHERE dept_id = '14420' AND crs_code IN (SELECT crs_code FROM teaching));
